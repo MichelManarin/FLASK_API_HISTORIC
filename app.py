50b1fffc7b94dc3bc405b248823ab934a0f9d640
Michel Manarin <michelmanarin@gmail.com>
2019-10-17 11:23:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask, request, url_for, render_template, jsonify, json, abort
+from flask_cors import CORS
 import db_controls as dbc
 
 app = Flask(__name__)
+
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
 @app.route("/historic", methods = ['GET'])
 def get_historics():
@@ -28,6 +32,9 @@ def not_found_error(e):
 def not_found_error(e):
     return "Error", 500
 
-if __name__ == "__main__":
+def main():
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
