2cadfda3c8b29941c01d515283dcd063f464234a
Michel Manarin <michelmanarin@gmail.com>
2019-10-17 11:04:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,6 @@ def not_found_error(e):
 def not_found_error(e):
     return "Error", 500
 
-if __name__ == '__main__':
-    app.run(debug = True, port = 8080)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
